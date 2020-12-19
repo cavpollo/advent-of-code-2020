55bdc1f3e347dedba24e14307191ab517ed7b83a
cavpollo <cavpollo@gmail.com>
2020-12-19 13:52:44 +0100

    Day 18 - Recursive math parser

---
+++ Day18A.java
@@ -0,0 +1,93 @@
+package advent;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day18A {
+    public static void main(String[] args) throws IOException {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Paste the input:");
+
+        final List<String> expressions = new ArrayList<>();
+        do {
+            final String readLine = bufferedReader.readLine();
+            if (readLine != null && !readLine.isEmpty()) {
+                expressions.add(readLine.trim());
+            } else {
+                break;
+            }
+        } while (true);
+
+        System.out.println("Got " + expressions.size() + " expressions");
+
+        final Instant start = Instant.now();
+
+        long total = 0;
+        for (final String expression : expressions) {
+//            System.out.println("calculating: " + expression);
+            total += calculate(expression);
+        }
+
+        final Instant finish = Instant.now();
+        final long timeElapsed = Duration.between(start, finish).toMillis();
+
+        System.out.println("Total of all expressions is " + total + " in " + timeElapsed + "ms");
+    }
+
+    private static long calculate(final String expression) {
+        Character operator = null;
+        Long result = null;
+        int parserPosition = 0;
+        do {
+            final long parsedValue;
+            int startParserPosition = parserPosition;
+            if (expression.charAt(parserPosition) == '(') {
+                int parenthesisCount = 0;
+                do {
+                    if (expression.charAt(parserPosition) == '(') {
+                        parenthesisCount++;
+                    } else if (expression.charAt(parserPosition) == ')') {
+                        parenthesisCount--;
+                    }
+                    parserPosition++;
+                } while (parenthesisCount > 0 && parserPosition < expression.length());
+
+                parsedValue = calculate(expression.substring(startParserPosition + 1, parserPosition - 1));
+            } else {
+                do {
+                    parserPosition++;
+                } while (parserPosition < expression.length() && expression.charAt(parserPosition) != ' ');
+
+                parsedValue = Long.parseLong(expression.substring(startParserPosition, parserPosition));
+            }
+
+            if (result == null) {
+                result = parsedValue;
+            } else {
+                if (operator.equals('*')) {
+//                    System.out.println((result * parsedValue) + " = " + result + " * " + parsedValue + " (OP)");
+                    result = result * parsedValue;
+                } else if (operator.equals('+')) {
+//                    System.out.println((result + parsedValue) + " = " + result + " + " + parsedValue + " (OP)");
+                    result = result + parsedValue;
+                } else {
+                    throw new RuntimeException("=( weird operator: '" + operator + "'");
+                }
+            }
+
+            if (parserPosition >= expression.length()) {
+//                System.out.println(result + " = " + expression + " (END)");
+                return result;
+            }
+
+            operator = expression.charAt(parserPosition + 1);
+            parserPosition = parserPosition + 3;
+        } while (true);
+    }
+}
